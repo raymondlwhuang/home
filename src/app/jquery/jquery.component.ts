b42ab0399cab9efea18bd0bee3cd0c11e0df74e5
raymondlwhuang <raymondlwhuang@yahoo.com>
2020-03-05 16:54:56 -0500

    add iframe for demo page

--- src/app/jquery/jquery.component.ts
+++ src/app/jquery/jquery.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-jquery',
   templateUrl: './jquery.component.html',
-  styleUrls: ['./jquery.component.css']
+  styleUrls: ['../javascript/javascript.component.css']
 })
 export class JqueryComponent implements OnInit {
 
